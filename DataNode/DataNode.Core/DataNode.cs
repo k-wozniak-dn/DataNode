e63d6bcd2ba199e13df31c04a2758899178b276e
Krzysztof Wozniak <krzysztof.roman.wozniak@gmail.com>
2026-04-06 14:43:50 +0200

    wip2

--- DataNode/DataNode.Core/DataNode.cs
+++ DataNode/DataNode.Core/DataNode.cs
@@ -22,13 +22,14 @@ public abstract record DnValue
 public record StringValue(string Value) : DnValue;
 public record IntegerValue(int Value) : DnValue;
 public record DecimalValue(decimal Value) : DnValue;
-public record Properties(Dictionary<string, DnValue> DnValueDictionary)
+public record Attributes(Dictionary<string, DnValue> DnValueDictionary, IParentDataNode Parent )
 {
-    public static Properties Create() => new([]);
+    public static Attributes Create(IParentDataNode parent) => new([], parent);
 
-    public string? GetString(string propertyName)
+    public string? GetString(string attributeName)
     {
-        if (DnValueDictionary.TryGetValue(propertyName, out DnValue? value))
+        attributeName = attributeName.ToUpper();
+        if (DnValueDictionary.TryGetValue(attributeName, out DnValue? value))
         {
             if (value is StringValue stringValue)
             {
@@ -38,9 +39,10 @@ public record Properties(Dictionary<string, DnValue> DnValueDictionary)
         return null;
     }
 
-    public int? GetInteger(string propertyName)
+    public int? GetInteger(string attributeName)
     {
-        if (DnValueDictionary.TryGetValue(propertyName, out DnValue? value))
+        attributeName = attributeName.ToUpper();
+        if (DnValueDictionary.TryGetValue(attributeName, out DnValue? value))
         {
             if (value is IntegerValue integerValue)
             {
@@ -50,9 +52,10 @@ public record Properties(Dictionary<string, DnValue> DnValueDictionary)
         return null;
     }
 
-    public decimal? GetDecimal(string propertyName)
+    public decimal? GetDecimal(string attributeName)
     {
-        if (DnValueDictionary.TryGetValue(propertyName, out DnValue? value))
+        attributeName = attributeName.ToUpper();
+        if (DnValueDictionary.TryGetValue(attributeName, out DnValue? value))
         {
             if (value is DecimalValue decimalValue)
             {
@@ -62,77 +65,149 @@ public record Properties(Dictionary<string, DnValue> DnValueDictionary)
         return null;
     }
 
-    public void Set(string propertyName, string value)
+    public void Set(string attributeName, string value)
     {
-        DnValueDictionary[propertyName] = value;
+        if (attributeName.Length > Parent.AttributeLengthLimit)
+        {
+            throw new ArgumentException($"Attribute name length exceeds the limit of {Parent.AttributeLengthLimit} characters.");
+        }
+        if ( value.Length > Parent.StringValueLengthLimit)
+        {
+            throw new ArgumentException($"String value length exceeds the limit of {Parent.StringValueLengthLimit} characters.");
+        }
+
+        attributeName = attributeName.ToUpper();
+
+        if (DnValueDictionary.Count >= Parent.AttributesCountLimit && !DnValueDictionary.ContainsKey(attributeName))
+        {
+            throw new InvalidOperationException($"Attributes count exceeds the limit of {Parent.AttributesCountLimit}.");
+        }
+        DnValueDictionary[attributeName] = value;
     }
 
-    public void Set(string propertyName, int value)
+    public void Set(string attributeName, int value)
     {
-        DnValueDictionary[propertyName] = value;
+        if (attributeName.Length > Parent.AttributeLengthLimit)
+        {
+            throw new ArgumentException($"Attribute name length exceeds the limit of {Parent.AttributeLengthLimit} characters.");
+        }
+
+        attributeName = attributeName.ToUpper();
+
+        if (DnValueDictionary.Count >= Parent.AttributesCountLimit && !DnValueDictionary.ContainsKey(attributeName))
+        {
+            throw new InvalidOperationException($"Attributes count exceeds the limit of {Parent.AttributesCountLimit}.");
+        }
+        DnValueDictionary[attributeName] = value;
     }
 
-    public void Set(string propertyName, decimal value)
+    public void Set(string attributeName, decimal value)
     {
-        DnValueDictionary[propertyName] = value;
+        if (attributeName.Length > Parent.AttributeLengthLimit)
+        {
+            throw new ArgumentException($"Attribute name length exceeds the limit of {Parent.AttributeLengthLimit} characters.");
+        }
+
+        attributeName = attributeName.ToUpper();
+
+        if (DnValueDictionary.Count >= Parent.AttributesCountLimit && !DnValueDictionary.ContainsKey(attributeName))
+        {
+            throw new InvalidOperationException($"Attributes count exceeds the limit of {Parent.AttributesCountLimit}.");
+        }
+        DnValueDictionary[attributeName] = value;
     }
 
-    public void Remove(string propertyName)
+    public void Remove(string attributeName)
     {
-        DnValueDictionary.Remove(propertyName);
+        attributeName = attributeName.ToUpper();
+        DnValueDictionary.Remove(attributeName);
     }
 
-    public bool Contains(string propertyName)
+    public bool Contains(string attributeName)
     {
-        return DnValueDictionary.ContainsKey(propertyName);
+        attributeName = attributeName.ToUpper();
+        return DnValueDictionary.ContainsKey(attributeName);
     }
 
-    public IEnumerable<KeyValuePair<string, DnValue>> AllProperties()
+    public IEnumerable<KeyValuePair<string, DnValue>> AllAttributes()
     {
         return DnValueDictionary.Select(kvp => new KeyValuePair<string, DnValue>(kvp.Key, kvp.Value));
     }
 
-    public Properties Copy()
+    public Attributes Copy(IParentDataNode parent)
     {
-        return new Properties(new Dictionary<string, DnValue>(DnValueDictionary));
+        return new Attributes(new Dictionary<string, DnValue>(DnValueDictionary), parent);
     }
 
     public int Count { get { return DnValueDictionary.Count; } }
 
 }
-public record Keys(Dictionary<string, Properties> PropertiesDictionary);
-public record Index(List<Properties> PropertiesIndex);
+public record Keys(Dictionary<string, Attributes> AttributesDictionary);
+public record Index(List<Attributes> AttributesIndex);
 
-public class DataNode
+public interface IParentDataNode
 {
+    short AttributeLengthLimit { get; }
+    short StringValueLengthLimit { get; }
+    short AttributesCountLimit { get; }
+
+    short KeyLengthLimit { get; }
+    short KeysCountLimit { get; }
+    short IndexCountLimit { get; }
+}
+
+public class DataNode : IParentDataNode
+{
+    #region Properties and Fields
     private readonly Keys Keys = new([]);
     private readonly Index Index = new([]);
+    public short AttributeLengthLimit { get { return 64; } }
+    public short AttributesCountLimit { get { return 8; } }
+    public short StringValueLengthLimit { get { return 256; } }    
+    public short KeyLengthLimit { get { return 64; } }
+    public short KeysCountLimit { get { return 72; } }
+    public short IndexCountLimit { get { return 64; } }
+    public int KeysCount { get { return Keys.AttributesDictionary.Count ;} }  
+    public int IndexCount { get { return Index.AttributesIndex.Count ;} } 
+
+    #endregion
+
     public DataNode() : base()
     {
     }
 
-    public int KeysCount { get { return Keys.PropertiesDictionary.Count ;} }  
-
-    public int IndexCount { get { return Index.PropertiesIndex.Count ;} } 
-
-    public IEnumerable<KeyValuePair<string, Properties>> AllKeys()
+    #region Keys
+    public IEnumerable<KeyValuePair<string, Attributes>> AllKeys()
     {
-        return [.. Keys.PropertiesDictionary.Select(kvp => new KeyValuePair<string, Properties>(kvp.Key, kvp.Value))];
-    }
-
-    public IEnumerable<KeyValuePair<int, Properties>> AllIndex()
-    {
-        return [.. Index.PropertiesIndex.Select((p, i) => new KeyValuePair<int, Properties>(i, p))];
+        return [.. Keys.AttributesDictionary.Select(kvp => new KeyValuePair<string, Attributes>(kvp.Key, kvp.Value))];
     }
     
     public bool ContainsKey(string key)
     {
-        return Keys.PropertiesDictionary.ContainsKey(key);
+        key = key.ToUpper();
+        return Keys.AttributesDictionary.ContainsKey(key);
     }
 
-    public Properties? Get(string key)
+    public void Set(string key, Attributes properties)
     {
-        if (Keys.PropertiesDictionary.TryGetValue(key, out Properties? properties))
+        key = key.ToUpper();
+
+        if (key.Length > KeyLengthLimit)
+        {
+            throw new ArgumentException($"Key length exceeds the limit of {KeyLengthLimit} characters.");
+        }
+        if (Keys.AttributesDictionary.Count >= KeysCountLimit && !ContainsKey(key))
+        {
+            throw new InvalidOperationException($"Keys count exceeds the limit of {KeysCountLimit}.");
+        }
+        Keys.AttributesDictionary[key] = properties;
+    }
+
+    public Attributes? Get(string key)
+    {
+        key = key.ToUpper();
+
+        if (Keys.AttributesDictionary.TryGetValue(key, out Attributes? properties))
         {
             return properties;
         }
@@ -142,121 +217,138 @@ public class DataNode
         }
     }
 
-    public Properties GetOrCreate(string key)
+    public Attributes GetOrCreate(string key)
     {
-        if (Keys.PropertiesDictionary.TryGetValue(key, out Properties? properties))
+        key = key.ToUpper();
+
+        var properties = Get(key);
+        if (properties != null)
         {
             return properties;
         }
         else
         {
-            Keys.PropertiesDictionary[key] = Properties.Create();
-            return Keys.PropertiesDictionary[key];
-        }
-    }
-
-    public Properties? Get(int index)
-    {
-        if (index >= 0 && index < Index.PropertiesIndex.Count)
-        {
-            return Index.PropertiesIndex[index];
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    public Properties GetOrCreate(int index)
-    {
-        if (index < 0)
-        {
-            Index.PropertiesIndex.Insert(0, Properties.Create());
-            return Index.PropertiesIndex[0];
-        }
-        else if (index >= Index.PropertiesIndex.Count)
-        {
-            Index.PropertiesIndex.Add(Properties.Create());
-            return Index.PropertiesIndex[^1];
-        }
-        else
-        {
-            return Index.PropertiesIndex[index];
-        }
-    }
-
-    public Properties GetOrCreate()
-    {
-        Index.PropertiesIndex.Add(Properties.Create());
-        return Index.PropertiesIndex[^1];
-    }
-
-    public Properties? GetAndRemove(int index)
-    {
-        if (index >= 0 && index < Index.PropertiesIndex.Count)
-        {
-            var properties = Index.PropertiesIndex[index];
-            Index.PropertiesIndex.RemoveAt(index);
+            properties = Attributes.Create(this);
+            Set(key, properties);
             return properties;
         }
-        else
-        {
-            return null;
-        }
-    }
-
-    public void Set(string key, Properties properties)
-    {
-        Keys.PropertiesDictionary[key] = properties;
-    }
-
-    public void Set(int index, Properties properties)
-    {
-        if (index < 0)
-        {
-            Index.PropertiesIndex[0] = properties;
-        }
-        else if (index >= Index.PropertiesIndex.Count)
-        {
-            Index.PropertiesIndex[^1] = properties;
-        }
-        else
-        {
-            Index.PropertiesIndex[index] = properties;
-        }
-    }
-
-    public void Insert(int index, Properties properties)
-    {
-        if (index < 0)
-        {
-            Index.PropertiesIndex.Insert(0, properties);
-        }
-        else if (index >= Index.PropertiesIndex.Count)
-        {
-            Index.PropertiesIndex.Add(properties);
-        }
-        else
-        {
-            Index.PropertiesIndex.Insert(index, properties);
-        }
-    }
-
-    public void Add(Properties properties)
-    {
-        Index.PropertiesIndex.Add(properties);
     }
 
     public void Remove(string key)
     {
-        Keys.PropertiesDictionary.Remove(key);
+        key = key.ToUpper();
+        Keys.AttributesDictionary.Remove(key);
+    }
+
+    #endregion
+
+    #region Index
+    public IEnumerable<KeyValuePair<int, Attributes>> AllIndex()
+    {
+        return [.. Index.AttributesIndex.Select((p, i) => new KeyValuePair<int, Attributes>(i, p))];
+    }
+    
+    public void Add(Attributes properties)
+    {
+        if (Index.AttributesIndex.Count >= IndexCountLimit)
+        {
+            throw new InvalidOperationException($"Index count exceeds the limit of {IndexCountLimit}.");
+        }
+        Index.AttributesIndex.Add(properties);
+    }
+
+    public Attributes Add()
+    {
+        Add(Attributes.Create(this));
+        return Index.AttributesIndex[^1];
+    }
+
+    public void Set(int index, Attributes properties)
+    {
+        if (index < 0)
+        {
+            Index.AttributesIndex[0] = properties;
+        }
+        else if (index >= Index.AttributesIndex.Count)
+        {
+            Index.AttributesIndex[^1] = properties;
+        }
+        else
+        {
+            Index.AttributesIndex[index] = properties;
+        }
+    }
+
+    public void Insert(int index, Attributes properties)
+    {
+        if (Index.AttributesIndex.Count >= IndexCountLimit)
+        {
+            throw new InvalidOperationException($"Index count exceeds the limit of {IndexCountLimit}.");
+        }
+
+        if (index < 0)
+        {
+            Index.AttributesIndex.Insert(0, properties);
+        }
+        else if (index >= Index.AttributesIndex.Count)
+        {
+            Add(properties);
+        }
+        else
+        {
+            Index.AttributesIndex.Insert(index, properties);
+        }
+    }
+
+    public Attributes? Get(int index)
+    {
+        if (index >= 0 && index < Index.AttributesIndex.Count)
+        {
+            return Index.AttributesIndex[index];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Attributes GetOrCreate(int index)
+    {
+        var properties = Get(index);
+        if (properties != null)
+        {
+            return properties;
+        }
+        else
+        {
+            properties = Attributes.Create(this);
+            Insert(index, properties);
+            return properties;
+        }
+    }
+
+    public Attributes? GetAndRemove(int index)
+    {
+        if (index >= 0 && index < Index.AttributesIndex.Count)
+        {
+            var properties = Index.AttributesIndex[index];
+            Index.AttributesIndex.RemoveAt(index);
+            return properties;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public void Remove(int index)
     {
-        if (index >= 0 && index < Index.PropertiesIndex.Count)
+        if (index >= 0 && index < Index.AttributesIndex.Count)
         {
-            Index.PropertiesIndex.RemoveAt(index);
+            Index.AttributesIndex.RemoveAt(index);
         }
-    }
+    }    
+
+    #endregion
+
 }
--- DataNode/DataNode.Tests/UnitTest1.cs
+++ DataNode/DataNode.Tests/UnitTest1.cs
@@ -6,20 +6,46 @@ namespace DataNode.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void SetProperties_Correctly()
+    public void Copy_Correctly()
     {
         // Arrange
         var dataNode = new Core.DataNode();
-        var item1 = new Properties([]);
+        var item1 = new Attributes([], dataNode);
 
         // Act
         item1.Set("Name", "Apples");
         item1.Set("Price", 1.5m);
         item1.Set("Quantity", 10);
-        var item2 = item1;
-        item2 = item1.Copy();
+
+        var item2 = item1.Copy(dataNode);
+        item2.Set("Brand", "Sunny");
+        item2.Set("Quantity", 20);
+
         dataNode.Set("item1", item1);
         dataNode.Set("item2", item2);
+
+        // Assert
+        var count = dataNode.AllKeys().Sum(kvp => kvp.Value.Count);
+        var total = dataNode.AllKeys().Sum(kvp => kvp.Value.GetDecimal("Price") * kvp.Value.GetInteger("Quantity"));
+        Assert.Equal(7, count); 
+        Assert.Equal(45m, total); 
+    }
+
+    [Fact]
+    public void GetOrCreateKey_Correctly()
+    {
+        // Arrange
+        var dataNode = new Core.DataNode();
+        var item1 = dataNode.GetOrCreate("item1") ;
+        var item2 = dataNode.GetOrCreate("item2") ;
+
+        // Act
+        item1.Set("Name", "Apples");
+        item1.Set("Price", 1.5m);
+        item1.Set("Quantity", 10);
+
+        item2.Set("Name", "Apples");
+        item2.Set("Price", 1.5m);
         item2.Set("Brand", "Sunny");
         item2.Set("Quantity", 20);
 
@@ -30,5 +56,29 @@ public class UnitTest1
         Assert.Equal(45m, total); 
     }
 
+    [Fact]
+    public void GetOrCreateIndex_Correctly()
+    {
+        // Arrange
+        var dataNode = new Core.DataNode();
+        var item1 = dataNode.GetOrCreate(0) ;
+        var item2 = dataNode.GetOrCreate(1) ;
+
+        // Act
+        item1.Set("Name", "Apples");
+        item1.Set("Price", 1.5m);
+        item1.Set("Quantity", 10);
+
+        item2.Set("Name", "Apples");
+        item2.Set("Price", 1.5m);
+        item2.Set("Brand", "Sunny");
+        item2.Set("Quantity", 20);
+
+        // Assert
+        var count = dataNode.AllIndex().Sum(kvp => kvp.Value.Count);
+        var total = dataNode.AllIndex().Sum(kvp => kvp.Value.GetDecimal("Price") * kvp.Value.GetInteger("Quantity"));
+        Assert.Equal(7, count); 
+        Assert.Equal(45m, total); 
+    }
 
 }
